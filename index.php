77ad7f262679297492bfa51bbd9aec384ce6118f
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-12-12 14:09:27 +0100

    Created variable with PHP

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+
+    $paragrafo = 'F1 moved to an all-new car concept for 2022, spearheaded by ground effect aerodynamics, overhauled bodywork and bigger tyres, while sticking with ultra-efficient 1.6-litre turbo-hybrid power units. One of the primary goals was to generate closer racing and increase overtaking opportunities, with Ferrari driver Sainz  who claimed his first pole position and race victory this year  highlighting that these boxes have been ticked.';
+
+    $titoloPagina = 'PHP Badwords';
+
+    $titolo = 'Primo titolo in PHP';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $titoloPagina ?></title>
+</head>
+<body>
+    <h1><?php echo $titolo ?></h1>
+    <p> <?php echo $paragrafo ?></p>
+</body>
+</html>
